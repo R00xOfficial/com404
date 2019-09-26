a14b384be6f841c3ca391a9370ec1337292e5c13
R00xOfficial <myleslawson.ml@gmail.com>
2019-09-26 14:35:30 +0000

    Added complete AE1

---
+++ AE1 - Programming Fundamentals.py
@@ -0,0 +1,7 @@
+# I need to display "Trick or Treat"
+print("Trick or Treat")
+# I need to give them a imput at set it to a verible i can later call on
+val = input()
+# I need to print "It will be goastly" and end ith there imput
+print("It will be a goastly " + val)
+
