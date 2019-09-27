18cc9eb98d4cb5d11a99071e7e480a91e1a7c1dc
R00xOfficial <myleslawson.ml@gmail.com>
2019-09-27 13:37:03 +0000

    Task Completed

---
+++ 1-basics/1-output/4-ascii-art/bot.py
@@ -0,0 +1,7 @@
+print ("Please enter a character for the eye ")
+var = input()
+print ("Beep's expression is now as follows:")
+print ("##########")
+print ("#  " + var + "  " + var +"  #")
+print ("#  ----  #")
+print ("##########")
---
+++ 1-basics/2-input/3-data-types/bot.py
@@ -0,0 +1,16 @@
+print("What is your name human?")
+name = input()
+print()
+print("How old are you (in years)?")
+age = int(input())
+print()
+print("How tall are you (in meters)?")
+height = float(input())
+print()
+print("How much do you weigh (in kilograms)?")
+weight = float(input())
+print()
+
+bmi = weight / (height ** 2)
+
+print (name + " you are "  + str(age) + " years old and your bmi is " + str(bmi) + ".")
