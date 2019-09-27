84284316238c59bd9e8ed0fb182dbd9a8339bb5d
R00xOfficial <myleslawson.ml@gmail.com>
2019-09-27 13:37:36 +0000

    Need to finish notes

---
+++ AE1 - Practice TCA/Q2.py
@@ -0,0 +1,9 @@
+# I need to display "What type of night be it tonight?"
+# I need to give them a imput at set it to a verible i can later call on
+var = input("What type of night be it tonight?")
+if var == "lightening":
+    print("It is time to be ALIVE!")
+elif var == "cloudy":
+    print("The sky is stirring but we must wait.")
+else:
+    print("Soon my precious. Soon.")
