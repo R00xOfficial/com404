b2d5da5bfb5af208e56574c96e4d6e507bee707c
R00xOfficial <myleslawson.ml@gmail.com>
2019-09-27 13:37:20 +0000

    Task Completed

---
+++ AE1 - Practice TCA/Q1.py
@@ -0,0 +1,5 @@
+# I need to display "Trick or Treat"
+# I need to give them a imput at set it to a verible i can later call on
+val = input("Trick or Treat")
+# I need to print "It will be goastly" and end ith there imput
+print("It will be a goastly " + val)
